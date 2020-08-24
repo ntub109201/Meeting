3b02f5f38972ad750fab21b673abd9aa5500ad8c
123 <nx01daniel@gmail.com>
2020-08-24 22:36:00 +0800

    When 更新

--- MyApplication2/app/src/main/java/com/example/myapplication2/Diary/DiaryWhenActivity.java
+++ MyApplication2/app/src/main/java/com/example/myapplication2/Diary/DiaryWhenActivity.java
@@ -3,11 +3,14 @@ package com.example.myapplication2.Diary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActivityOptions;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -15,7 +18,12 @@ import com.example.myapplication2.Diary.DiaryHowPackage.DiaryHowActivity;
 import com.example.myapplication2.DiaryValue;
 import com.example.myapplication2.R;
 
+import java.util.Calendar;
+
 public class DiaryWhenActivity extends AppCompatActivity {
+
+    private TextView txtDate;
+    private Button btnDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,7 +73,29 @@ public class DiaryWhenActivity extends AppCompatActivity {
             }
         });
 
+        btnDate = findViewById(R.id.btnDate);
+        txtDate = findViewById(R.id.txtDate);
+        btnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar now = Calendar.getInstance();
+                DatePickerDialog datePickerDialog = new DatePickerDialog(DiaryWhenActivity.this,datePickerDlgOnDateSet,now.get(Calendar.YEAR),now.get(Calendar.MONTH),now.get(Calendar.DAY_OF_MONTH));
+                datePickerDialog.setTitle("日記日期");
+                datePickerDialog.setMessage("請選擇日期");
+                datePickerDialog.setIcon(android.R.drawable.ic_dialog_info);
+                datePickerDialog.setCancelable(false);
+                datePickerDialog.show();
+            }
+        });
+
     }
+
+    private DatePickerDialog.OnDateSetListener datePickerDlgOnDateSet = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            txtDate.setText(String.valueOf(year)+"/"+String.valueOf(month+1)+"/"+String.valueOf(dayOfMonth));
+        }
+    };
     // 擋住手機上回上一頁鍵
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
