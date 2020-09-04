1f35ca9e2708ca2cf99c71592915894d47d3f6f1
123 <nx01daniel@gmail.com>
2020-09-04 18:19:38 +0800

    問職業介面 完成

---
+++ MyApplication2/app/src/main/java/com/example/myapplication2/Login/FirstLoginActivity2.java
@@ -0,0 +1,94 @@
+package com.example.myapplication2.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.example.myapplication2.MainActivity;
+import com.example.myapplication2.R;
+import com.example.myapplication2.sqlReturn;
+
+public class FirstLoginActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_login2);
+
+        final ImageButton imageButton1 = findViewById(R.id.imageButton1);
+        imageButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlReturn.PersonalJob = "上班族";
+                Intent intent = new Intent(FirstLoginActivity2.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("id",1);
+                startActivity(intent);
+            }
+        });
+
+        final ImageButton imageButton2 = findViewById(R.id.imageButton2);
+        imageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlReturn.PersonalJob = "餐旅業";
+                Intent intent = new Intent(FirstLoginActivity2.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("id",1);
+                startActivity(intent);
+            }
+        });
+
+        final ImageButton imageButton3 = findViewById(R.id.imageButton3);
+        imageButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlReturn.PersonalJob = "製造業";
+                Intent intent = new Intent(FirstLoginActivity2.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("id",1);
+                startActivity(intent);
+            }
+        });
+
+        final ImageButton imageButton4 = findViewById(R.id.imageButton4);
+        imageButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlReturn.PersonalJob = "學生";
+                Intent intent = new Intent(FirstLoginActivity2.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("id",1);
+                startActivity(intent);
+            }
+        });
+
+        final ImageButton imageButton5 = findViewById(R.id.imageButton5);
+        imageButton5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlReturn.PersonalJob = "娛樂業";
+                Intent intent = new Intent(FirstLoginActivity2.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("id",1);
+                startActivity(intent);
+            }
+        });
+
+        final ImageButton imageButton6 = findViewById(R.id.imageButton6);
+        imageButton6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlReturn.PersonalJob = "其他";
+                Intent intent = new Intent(FirstLoginActivity2.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("id",1);
+                startActivity(intent);
+            }
+        });
+    }
+}
